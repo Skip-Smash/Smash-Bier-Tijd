fcc85d13863f1c7a7ced648d4fbb0719a2f1b5ef
Skip-Smash <skip@smashstudios.nl>
2025-12-11 16:06:44 +0100

    feat: Convert app to standalone HTML file
    This change refactors the Smash Studios Beer Time application to be a single, self-contained HTML file. All necessary JavaScript, CSS, and PWA configurations are now embedded directly within `index.html`. This simplifies deployment and eliminates the need for a complex build process or separate script/style files, making it easier to host as a static asset.

--- index.tsx
+++ index.tsx
@@ -1,16 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// This file is no longer used in the standalone version.
+// The code has been moved to index.html for easier static deployment.
